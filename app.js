6d149010dcdaa06ec7580cc3a5122ed69c24693e
Yzwcp <1774570823@qq.com>
2022-09-24 12:27:55 +0800

    dist

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const Koa = require('koa')
 const app = new Koa()
 const router = require('./src/route/index.js')
-const cors = require('@koa/cors')
 const bodyParser = require('koa-bodyparser')
+const serve = require('koa-static');
+const path =require("path")
 const ws = require('./src/websocket/index')
+var cors = require('koa2-cors');
+
 app.use(cors())
+app.use(serve('dist'))
 app.use(bodyParser())
 app.use(router.routes());//启动路由
 app.use(router.allowedMethods());
